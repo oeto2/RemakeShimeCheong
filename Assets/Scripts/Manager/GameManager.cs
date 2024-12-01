fdb85596443d4ccdbdb88ba4b71279c9ac6876c1
oeto2 <oeto2@naver.com>
2024-12-01 22:40:29 +0900

    Feat : 마우스 커서 변경 시스템 구현 완료

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,4 +1,3 @@
-
 //게임 매니저
 
 using System;
@@ -8,23 +7,36 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
-    
     //커서 세팅 관련
     public Texture2D cursorTexture_Idle;
-    public Texture2D cursorTexture_Click; 
-    private CursorMode cursorMode = CursorMode.Auto;
+    public Texture2D cursorTexture_Click;
+    private CursorMode cursorMode = CursorMode.ForceSoftware;
     private Vector2 hotSpot = Vector2.zero;
-    
+
     private void Awake()
     {
         Init();
+    }
+
+    private void Update()
+    {
+        //마우스 상태에 따른 커서 이미지 변경
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeCursor(cursorTexture_Click);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            ChangeCursor(cursorTexture_Idle);
+        }
     }
 
     public override void Init()
     {
         base.Init();
         SettingCursor(); //커서 세팅
-        ChangeCursor(); //커서 변경
+        ChangeCursor(cursorTexture_Idle); //기본 커서 변경
     }
 
     //커서 세팅하기
@@ -33,20 +45,19 @@ public class GameManager : Singleton<GameManager>
         cursorTexture_Idle = ResourceManager.Instance.Load<Texture2D>(ResourceImagePath.Cursor_Idle);
         cursorTexture_Click = ResourceManager.Instance.Load<Texture2D>(ResourceImagePath.Cursor_Click);
     }
-    
+
     //커서 변경하기
-    public void ChangeCursor()
+    public void ChangeCursor(Texture2D cursorTexture_)
     {
-        ConsoleLogger.Log("커서 변경완료");
-        Cursor.SetCursor(cursorTexture_Idle, hotSpot, cursorMode); //커서 변경
+        Cursor.SetCursor(cursorTexture_, hotSpot, cursorMode); //커서 변경
     }
-    
+
     //씬 전환하기
     public void ChangeScene(UnitySceneName sceneName_)
     {
-        SceneManager.LoadScene(sceneName_.ToString()); 
+        SceneManager.LoadScene(sceneName_.ToString());
     }
-    
+
     //게임 종료
     public void ExitGame()
     {
