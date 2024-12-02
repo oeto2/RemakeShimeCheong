29b869a7dbb6fda125bac6a0d9677e2eb30b247f
oeto2 <oeto2@naver.com>
2024-12-02 22:13:07 +0900

    Feat : 플레이어 이동 구현

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+//플레이어 애니메이션
+public class PlayerAnimation : MonoBehaviour
+{
+    // 변수 : 애니메이션 관련
+    public Animator playerAnimator;
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,10 +1,33 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerInputSystem : MonoBehaviour
+//플레이어 조작
+public class PlayerController : MonoBehaviour
 {
     private PlayerInput _playerInput;
-    
+    private PlayerMovement _playerMovement;
+
+    private void Awake()
+    {
+        _playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    //플레이어 이동키 입력
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            //플레이어 이동 방향 설정
+            _playerMovement.ApplyMoveVelocity(context.ReadValue<Vector2>());
+        }
+
+        if (context.canceled)
+        {
+            //플레이어 이동 정지
+            _playerMovement.ApplyMoveVelocity(Vector2.zero);
+        }
+    }
+
     //지도 단축키 입력
     public void OnMap(InputAction.CallbackContext context)
     {
@@ -13,7 +36,7 @@ public class PlayerInputSystem : MonoBehaviour
             ConsoleLogger.Log("지도 버튼 입력");
         }
     }
-    
+
     //대화 단축키 입력
     public void OnTalk(InputAction.CallbackContext context)
     {
@@ -22,7 +45,7 @@ public class PlayerInputSystem : MonoBehaviour
             ConsoleLogger.Log("대화 버튼 입력");
         }
     }
-    
+
     //인벤토리 단축키 입력
     public void OnInventory(InputAction.CallbackContext context)
     {
@@ -31,7 +54,7 @@ public class PlayerInputSystem : MonoBehaviour
             ConsoleLogger.Log("인벤토리 버튼 입력");
         }
     }
-    
+
     //포탈 단축키 입력
     public void OnPortal(InputAction.CallbackContext context)
     {
@@ -40,7 +63,7 @@ public class PlayerInputSystem : MonoBehaviour
             ConsoleLogger.Log("포탈 버튼 입력");
         }
     }
-    
+
     //옵션 키 입력
     public void OnOption(InputAction.CallbackContext context)
     {
@@ -49,9 +72,9 @@ public class PlayerInputSystem : MonoBehaviour
             ConsoleLogger.Log("옵션 버튼 입력");
         }
     }
-    
+
     //상호작용 키 입력
-    public void OnInteration(InputAction.CallbackContext context)
+    public void OnInteraction(InputAction.CallbackContext context)
     {
         if (context.started)
         {
