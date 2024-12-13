3f56a19e28ec4d0291898d8ac77c9c6326eb6cad
oeto2 <oeto2@naver.com>
2024-12-13 22:06:38 +0900

    Feat : 토스트 메세지 구현 및 맵에 있는 아이템 획득 구현

--- Assets/Scripts/Object/ItemObject.cs
+++ Assets/Scripts/Object/ItemObject.cs
@@ -10,30 +10,47 @@ public interface Iinteractable
 
 public class ItemObject : MonoBehaviour, Iinteractable
 {
-    public int itemId = 0; //획득할 Item ID
     private Inventory _inventory;
     private ToastMessagePopup _toastMessage;
+    private bool _hasAcquiredItem = false; //중복 획득 방지
+    
+    //변수 : 세팅 
+    [Header("Setting")] 
+    public int itemId = 0; //획득할 Item ID
+    [SerializeField] private bool isHide = false; //획득 시 오브젝트를 숨길 것인지
 
     private void Start()
     {
         _inventory = UIManager.Instance.GetPopupObject<InventoryPopup>().GetComponent<Inventory>();
         _toastMessage = UIManager.Instance.GetUIComponent<ToastMessagePopup>();
     }
-  
+
     //상호작용 시
     public void OnInteract()
     {
-        _inventory.GetItem(itemId); //아이템 획득
-        ItemData itemData = _inventory.GetInventoryItemData(itemId);
-        UIManager.Instance.ShowPopup<ToastMessagePopup>(); //토스트 메세지 띄우기
-        _toastMessage.SetToastMessage(ResourceManager.Instance.Load<Sprite>(itemData.SpritePath), itemData.Name, 
-            $"{itemData.Name} 획득"); //토스트 메세지 설정
-        gameObject.SetActive(false);
+        //한번도 아이템을 획득한 적이 없다면
+        if (!_hasAcquiredItem)
+        {
+            //아이템 획득
+            _inventory.GetItem(itemId);
+            _hasAcquiredItem = true;
+            ItemData itemData = _inventory.GetInventoryItemData(itemId);
+
+            //토스트 메세지
+            UIManager.Instance.ShowPopup<ToastMessagePopup>();
+            _toastMessage.StartHideMessageAnimation();
+            _toastMessage.SetToastMessage(ResourceManager.Instance.Load<Sprite>(itemData.SpritePath), itemData.Name,
+                $"{itemData.Name} 획득");
+            _toastMessage.StartOnMessageAnimation();
+
+            //오브젝트 비활성화
+            if (isHide)
+                gameObject.SetActive(false);
+        }
     }
 
     //플레이어와 닿았을 경우
     public void OnPlayerCollision()
     {
-        
     }
 }
